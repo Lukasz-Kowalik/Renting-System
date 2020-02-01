a52a0d6bfa8478fb2f012d60fdb4a75c225d2041
Lukasz-Kowalik <lukasz.kowalik41@gmail.com>
2020-02-01 22:25:23 +0100

    added UserPanel view

---
+++ Renting System/Controllers/UserPanelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Renting_System.Controllers
+{
+    public class UserPanelController : Controller
+    {
+        public IActionResult Item()
+        {
+            return View();
+        }
+        public IActionResult Rented()
+        {
+            return View();
+        }
+    }
+}
