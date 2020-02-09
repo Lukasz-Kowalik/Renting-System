96dd3adf614ae85cf87d14132a7a66361b29654a
Lukasz-Kowalik <lukasz.kowalik41@gmail.com>
2020-02-09 18:50:54 +0100

    added data logic models

---
+++ DataLogic/Model/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataLogic.Model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsLogin { get; set; }
+        public AccountTypes AccountType { get; set; }
+
+        public enum AccountTypes
+        {
+            Visitor,
+            User,
+            Worker,
+            Admin
+        }
+    }
+}
