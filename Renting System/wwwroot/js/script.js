c519e9a9c090398f815e9dcdcafb353537fef4c6
Lukasz-Kowalik <lukasz.kowalik41@gmail.com>
2020-02-14 21:33:55 +0100

    small changes

--- Renting System/wwwroot/js/script.js
+++ Renting System/wwwroot/js/script.js
@@ -1,4 +1,30 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+window.onload = function() {
+  $(document).ready(function(e) {
+    $('#loginBtn').on('click', validate);
+  });
+};
 
-// Write your JavaScript code.
+function validate() {
+  const password = $('#password').val();
+  const email = $('#email').val();
+}
+function showIfIsValidate(object) {
+  $(object).addClass('is-valid');
+}
+function showIfIsNotValidate(object) {
+  $(object).addClass('is-invalid');
+}
+function validateEmail(email) {
+  // const re = /^(([^<>()[]\\.,;:s@"]+(.[^<>()[]\\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+  const regex = new RegExp(
+    '/^(([^<>()[]\\.,;:s@"]+(.[^<>()[]\\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/'
+  );
+  return regex.test(email);
+}
+
+function isMediumPassword(pass) {
+  const mediumRegex = new RegExp(
+    '^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})'
+  );
+  return mediumRegex.test(pass);
+}
