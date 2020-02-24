3983bf03317a240d484cbb95f4575797a4887f5e
Lukasz-Kowalik <lukasz.kowalik41@gmail.com>
2020-02-25 00:26:28 +0100

    added context

---
+++ RentingSystemAPI/Model/DataBaseInit.cs
@@ -0,0 +1,13 @@
+﻿using DataLogic.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace RentingSystemAPI.Model
+{
+    public static class DatabaseInit
+    {
+    }
+}
