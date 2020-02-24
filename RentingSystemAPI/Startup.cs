2f0e5ac1f86a65358620a4a6c0e9286eec31af2f
Lukasz-Kowalik <lukasz.kowalik41@gmail.com>
2020-02-25 00:23:29 +0100

    configured connection to the database

--- RentingSystemAPI/Startup.cs
+++ RentingSystemAPI/Startup.cs
@@ -20,6 +20,20 @@ namespace RentingSystemAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var server = Configuration["DBServer"] ?? "DataBaseSQL";
+            var port = Configuration["DBPort"] ?? "1433";
+            //using SA isn't good on production, better practice would be using account with lower permission
+            var user = Configuration["DBUser"] ?? "SA";
+            var password = Configuration["DBPassword"] ?? "Password2020";
+            var database = Configuration["Database"] ?? "Renting";
+            services.AddDbContext<RentingContext>(options => options.UseSqlServer(
+                                                                                    $"Server={server},{port};" +
+                                                                                               $"Initial Catalog={database};" +
+                                                                                               $"User ID={user};" +
+                                                                                               $"Password={password}"));
+            services.AddDbContext<RentingContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("RentingDb")));
+
             services.AddControllers();
         }
 
@@ -30,6 +44,8 @@ namespace RentingSystemAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            DatabaseInit.InitDataBase(app);
 
             app.UseHttpsRedirection();
 
