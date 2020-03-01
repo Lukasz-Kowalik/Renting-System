68e494ebc616a88b4212bb20f15365a2016d9407
Lukasz-Kowalik <lukasz.kowalik41@gmail.com>
2020-03-01 13:47:02 +0100

    updated rent model

--- DataLogic/Model/Rent.cs
+++ DataLogic/Model/Rent.cs
@@ -20,8 +20,56 @@ namespace DataLogic
         public int ItemId { get; set; }
 
         public Item Item { get; set; }
-        public int Count { get; set; }
-        public DateTime RentTime { get; set; }
-        public DateTime RentReturn { get; set; }
+        public int Quantity { get; set; }
+        public int MaxReturnTimeInDays { get; set; } = 7;
+
+        public DateTime RentTime { get; set; } = DateTime.Now;
+
+        public DateTime WhenShouldBeReturned { get; set; } = DateTime.Now.AddDays(7);
+
+        public DateTime? RentReturnTime { get; set; } = null;
+
+        public Rent()
+        {
+        }
+
+        public Rent(int userId, int itemId, int quantity, int maxReturnTimeInDays = 7)
+        {
+            UserId = userId;
+            ItemId = itemId;
+            Quantity = quantity;
+            MaxReturnTimeInDays = maxReturnTimeInDays;
+        }
+
+        public Rent(int userId, int itemId, int quantity,
+            DateTime rentTime, DateTime rentReturnTime, DateTime whenShouldBeReturned)
+        {
+            UserId = userId;
+            ItemId = itemId;
+            Quantity = quantity;
+            RentTime = rentTime;
+            RentReturnTime = rentReturnTime;
+            WhenShouldBeReturned = whenShouldBeReturned;
+        }
+
+        public Rent(int userId, int itemId, int quantity,
+            DateTime rentTime, DateTime rentReturnTime
+           )
+        {
+            UserId = userId;
+            ItemId = itemId;
+            Quantity = quantity;
+            RentTime = rentTime;
+            RentReturnTime = rentReturnTime;
+        }
+
+        public Rent(int userId, int itemId, int quantity,
+            DateTime rentTime)
+        {
+            UserId = userId;
+            ItemId = itemId;
+            Quantity = quantity;
+            RentTime = rentTime;
+        }
     }
 }
