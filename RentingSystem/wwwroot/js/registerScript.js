4dbf494f944e18e1dfcd17d00f8523d2621e978f
Lukasz-Kowalik <lukasz.kowalik41@gmail.com>
2020-03-14 13:29:38 +0100

    added register script to get data from the form

---
+++ RentingSystem/wwwroot/js/registerScript.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    const x = $("#registerForm").serialize();
+    $("#registerForm").submit(function () {
+    const jqxhr = $.post('localhost:8000/CreateUser', $('#registerForm').serialize())
+        .success(
+            function() {
+                const loc = jqxhr.getResponseHeader('Location');
+                console.log(jqxhr);
+                console.log(loc);
+            }).error(function() {
+            $('#message').html("Error posting the update.");
+        });
+    return false;
+});
+});
