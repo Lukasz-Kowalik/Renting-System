d2c8c4287ffec82e5035ed009918c2b6c89bd6a9
Lukasz-Kowalik <lukasz.kowalik41@gmail.com>
2020-03-23 00:34:09 +0100

    updated styles

--- RentingSystem/wwwroot/js/script.js
+++ RentingSystem/wwwroot/js/script.js
@@ -1,21 +1,22 @@
-$(document).ready(function () {
-    function SendFormPost(webSubpage, form) {
-        $(form).submit(function (event) {
-            const data = $(form).serialize();
-            $.ajax({
-                type: 'POST',
-                url: 'http://localhost:8000/' + webSubpage,
-                contentType: "application/json; charset=utf-8",
-                dataType: "json",
-                data: JSON.stringify(data),
-                success: function (msg) {
-                }
-            });
-        });
-    }
+//$(document).ready(function () {
+//    function SendFormPost(webSubpage, form) {
+//        $(form).submit(function (event) {
+//            const data = $(form).serialize();
+//            $.ajax({
+//                type: 'POST',
+//                url: 'http://localhost:8000/' + webSubpage,
+//                contentType: "application/json; charset=utf-8",
+//                dataType: "json",
+//                data: JSON.stringify(data),
+//                success: function (msg) {
+//                    console.log(msg);
+//                }
+//            });
+//        });
+//    }
 
-    SendFormPost('#registerForm', 'CreateUser');
-});
+//    SendFormPost('#registerForm', 'CreateUser');
+//});
 //window.onload = function() {
 //  $(document).ready(function(e) {
 //    $('#loginBtn').on('click', validate);
