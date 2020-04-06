ededefa2f916d4926eff0c9ec4839b8d2fa2e704
Lukasz-Kowalik <lukasz.kowalik41@gmail.com>
2020-04-06 13:02:43 +0200

    fixed typo

--- DALTests/PasswordHashTest.cs
+++ DALTests/PasswordHashTest.cs
@@ -8,11 +8,11 @@ namespace DataLogicTests
         [Theory]
         [InlineData("password", "password", true)]
         [InlineData("password", "password1", false)]
-        public void Encrypt_PasswordIsEncryptedProperly_CheckIfPasswordsAreThisSame(string pas, string pas2, bool expectetResult)
+        public void Encrypt_PasswordIsEncryptedProperly_CheckIfPasswordsAreThisSame(string pas, string pas2, bool expectedResult)
         {
             var passwordHash = new PasswordHash(pas);
             var result = passwordHash.Verify(pas2);
-            Assert.Equal(result, expectetResult);
+            Assert.Equal(result, expectedResult);
         }
     }
 }
