77c19e5cf77868574359b02ab5a6a22ebb033fcd
Lukasz-Kowalik <lukasz.kowalik41@gmail.com>
2020-04-06 12:03:00 +0200

    added queries for get user/s

---
+++ RentingSystemAPI/Queries/GetAllUsersQuery.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using DAL.Models;
+using MediatR;
+
+namespace RentingSystemAPI.Controllers
+{
+    public class GetAllUsersQuery:IRequest<List<User>>
+    {
+        public GetAllUsersQuery()
+        {
+        }
+    }
+}
---
+++ RentingSystemAPI/Queries/GetUserByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL.Models;
+using MediatR;
+
+namespace RentingSystemAPI.Queries
+{
+    public class GetUserByIdQuery:IRequest<User>
+    {
+        public int UserId { get;  }
+
+        public GetUserByIdQuery(int userId)
+        {
+            UserId = userId;
+        }
+    }
+}
