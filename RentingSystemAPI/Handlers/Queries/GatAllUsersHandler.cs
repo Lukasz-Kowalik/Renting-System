ed130d525bcdc8793120f6a96d4df4626a409801
Lukasz-Kowalik <lukasz.kowalik41@gmail.com>
2020-04-06 12:08:37 +0200

    added queries hadnler for user

---
+++ RentingSystemAPI/Handlers/Queries/GatAllUsersHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DAL.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RentingSystemAPI.Controllers;
+using RentingSystemAPI.Model;
+
+namespace RentingSystemAPI.Handlers.Queries
+{
+    public class GatAllUsersHandler:IRequestHandler<GetAllUsersQuery,List<User>>
+    {
+        private readonly RentingContext _context;
+       
+
+        public GatAllUsersHandler(RentingContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<User>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _context.Users.ToListAsync();
+            return result;
+        }
+    }
+}
---
+++ RentingSystemAPI/Handlers/Queries/GetUserByIdHandler.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RentingSystemAPI.Model;
+using RentingSystemAPI.Queries;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RentingSystemAPI.Handlers.Queries
+{
+    public class GetUserByIdHandler : IRequestHandler<GetUserByIdQuery, User>
+    {
+        private readonly RentingContext _context;
+
+        public GetUserByIdHandler(RentingContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Users.FirstOrDefaultAsync(t => t.Id == request.UserId);
+        }
+    }
+}
