c827163e15ca5fcb0d0e133160ed7dd8d5aa83c5
Lukasz-Kowalik <lukasz.kowalik41@gmail.com>
2020-07-13 00:02:54 +0200

    added mapper for api

---
+++ RentingSystemAPI/DTOs/UserDto.cs
@@ -0,0 +1,17 @@
+﻿namespace RentingSystemAPI.DTOs
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Salt { get; set; }
+
+        //public string UserName
+        //{
+        //    get => UserName;
+        //    set => UserName = Email;
+        //}
+    }
+}
---
+++ RentingSystemAPI/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RentingSystemAPI.BAL.Entities;
+using RentingSystemAPI.DTOs;
+
+namespace RentingSystemAPI.Mapping
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<UserDto, User>()
+                .ForMember(u => u.UserName, m => m.MapFrom(u => u.Email));
+        }
+    }
+}
